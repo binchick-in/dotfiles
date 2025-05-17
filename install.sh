9261d41640e8fba33ac98c43d76856be7f566c89
binchick-in <me@billben.net>
2025-05-16 22:30:36 -0700

    Adding base directories

--- install.sh
+++ install.sh
@@ -2,17 +2,30 @@
 
 install_bins() {
     cp -r bin/ ~/
+    echo "Installed: ~/bin"
 }
 
 install_tmux() {
     cp .tmux.conf ~/
+    echo "Installed: ~/.tmux.conf"
 }
 
 
+ensure_directories() {
+    for dir in repos worktrees playground inbound; do
+        if [ ! -d ~/"$dir" ]; then
+            mkdir -p ~/"$dir"
+            echo "Created directory: ~/$dir"
+        else
+            echo "Directory already exists: ~/$dir"
+        fi
+    done
+}
+
 main() {
+    ensure_directories
     install_bins
     install_tmux
 }
-
 
 main
